d4c861becb661335f6bd47f51f2b9e8eb22540e9
jamalgillis <59495728+jamalgillis@users.noreply.github.com>
2023-08-13 04:15:53 -0500

    made some more of those updates

--- src/app/watch/components/WatchList.ts
+++ src/app/watch/components/WatchList.ts
@@ -11,10 +11,10 @@ const currentPagePath = '/'; // Replace this with the actual pathname of the cur
     { title: 'Contact', path: '/contact' },
   ];
 
-const router = useRouter();
+
 
 
 
 export default function WatchList() {
-  
+  const router = useRouter();
 }
